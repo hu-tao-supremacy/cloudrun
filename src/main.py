0b4595f0888eb7ad0ef97f717284548cf2ab5253
Norapat Buppodom <new17353@gmail.com>
2021-04-30 18:45:01 +0700

    fix: try flush python log

--- src/main.py
+++ src/main.py
@@ -3,19 +3,13 @@ import os
 from flask import Flask, request
 from personalization import personalization
 import db_model
-import logging as log
-import google.cloud.logging as logging
-
-logging_client = logging.Client()
-logging_client.setup_logging()
 
 app = Flask(__name__)
 
 
-
-log.info(f"Some log here") 
-
 print('starting')
+import sys
+sys.stdout.flush()
 
 @app.route("/")
 def on_event_change():
