9462667348159feff466d7f6a6fe70f19a48a469
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-02 22:06:19 +0700

    feat(main.py): add `/pubsub`

--- src/main.py
+++ src/main.py
@@ -13,10 +13,34 @@ import sys
 from flask import Flask, request
 from personalization import personalization
 import db_model
+import base64
+import json
 
 app = Flask(__name__)  
 
 print('starting')
+
+@app.route("/pubsub", methods=["POST"])
+def index():
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    pubsub_message = envelope["message"]
+
+    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
+        event_id = base64.b64decode(pubsub_message["eventId"]).decode("utf-8").strip()
+
+    print(f"Pub/Sub {event_id}!")
+
+    return ("", 204)
 
 @app.route("/<event_id>")
 def on_event_change(event_id):
